3dab97b2a2f55df7dd349e3b2161f70d40f3e2cd
Pedrosft <96483295+Pedrosft@users.noreply.github.com>
2025-02-01 17:00:36 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// FUNÇAO BUTÃO
+let ul = document.querySelector('nav .ul')
+function openMenu(){
+    ul.classList.add('open')
+}
+function closeMenu(){
+    ul.classList.remove('open')
+}
+// FIM FUNÇAO BUTÃO
